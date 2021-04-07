739714a7c8355ef491e7f063d0979bf7e4655248
Sanmathi05 <52269862+Sanmathi05@users.noreply.github.com>
2021-04-07 17:19:55 +0530

    Lab1/

---
+++ ex1.java
@@ -0,0 +1,28 @@
+package Lab1;
+
+import java.util.Scanner;
+
+public class Sumofcubes {
+
+	public static void main(String[] args) {
+		int no= 0,i;
+		Scanner s=new Scanner (System.in);
+		System.out.println("digit numbers");
+		int n=s.nextInt();
+		s.close();
+		
+		for(i=1;i<=n;i++) {
+			
+			no+=(i*i*i);
+			
+			
+		}
+		
+		System.out.println("sum of the cubes:" +no);
+
+
+	}
+
+	}
+
+
